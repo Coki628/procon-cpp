13e9fee127f297b03bc980f8b7f3378dcb1f2f8a
Coki <srcn4frdm.c@gmail.com>
2019-10-18 15:38:12 +0800

    定形更新

--- tpl.cpp
+++ tpl.cpp
@@ -14,12 +14,47 @@ typedef vector<vector<int>> vvi;
 #define str to_string
 #define mkp make_pair
 #define mkt make_tuple
-#define print(out) cout << (out) << endl
+// #define print(out) cout << (out) << endl
 const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
 void chmax(ll &a, ll b) { a = max(a, b); }
 void chmin(ll &a, ll b) { a = min(a, b); }
 
+template<typename Any>
+void print(Any out) {
+    cout << out << '\n';
+}
+
+void print(vector<ll> A) {
+    rep(i, 0, A.size()) {
+        cout << A[i];
+        cout << (i == A.size()-1 ? '\n' : ' ');
+    }
+}
+
+ll max(vector<ll> A) {
+    ll res = -INF;
+    for (ll a: A) chmax(res, a);
+    return res;
+}
+
+ll min(vector<ll> A) {
+    ll res = INF;
+    for (ll a: A) chmin(res, a);
+    return res;
+}
+
+ll max(ll A[], int len) {
+    ll res = -INF;
+    rep(i, 0, len) chmax(res, A[i]);
+    return res;
+}
+
+ll min(ll A[], int len) {
+    ll res = INF;
+    rep(i, 0, len) chmin(res, A[i]);
+    return res;
+}
 
 int main() {
     cin.tie(0);
