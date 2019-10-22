1f7156711dd849eeed7fe4622859a19791cecf37
Coki <srcn4frdm.c@gmail.com>
2019-10-22 17:51:11 +0800

    こどふぉ参戦, 1236,1248

---
+++ cdf1248d2.cpp
@@ -0,0 +1,121 @@
+/**
+ * ・map管理してたやつを普通に最小値だけカウントする感じに。
+ * ・AC0.1秒。
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef tuple<ll, int, int> tlii;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define all(obj) (obj).begin(), (obj).end()
+#define pb push_back
+#define str to_string
+#define mkp make_pair
+#define mkt make_tuple
+// #define print(out) cout << (out) << endl
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+void chmax(ll &a, ll b) { a = max(a, b); }
+void chmin(ll &a, ll b) { a = min(a, b); }
+
+template<typename Any>
+void print(Any out) {
+    cout << out << '\n';
+}
+
+template<typename T1, typename T2>
+void print(pair<T1, T2> out) {
+    cout << out.first << ' ' << out.second << '\n';
+}
+
+void print(vector<ll> A) {
+    rep(i, 0, A.size()) {
+        cout << A[i];
+        cout << (i == A.size()-1 ? '\n' : ' ');
+    }
+}
+
+ll max(vector<ll> A) {
+    ll res = -INF;
+    for (ll a: A) chmax(res, a);
+    return res;
+}
+
+ll min(vector<ll> A) {
+    ll res = INF;
+    for (ll a: A) chmin(res, a);
+    return res;
+}
+
+ll max(ll A[], int len) {
+    ll res = -INF;
+    rep(i, 0, len) chmax(res, A[i]);
+    return res;
+}
+
+ll min(ll A[], int len) {
+    ll res = INF;
+    rep(i, 0, len) chmin(res, A[i]);
+    return res;
+}
+
+int N, res, mx;
+string S;
+pii ans;
+
+// 最小値になった回数を返す
+int check(string S) {
+    int cur = 0, mn = 0, cnt = 0;
+    for (char c : S) {
+        if (c == '(') {
+            cur++;
+        } else {
+            cur--;
+        }
+        if (cur < mn) {
+            mn = cur;
+            cnt = 1;
+        } else if (cur == mn) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cin >> N;
+    cin >> S;
+    mx = 0;
+    ans = {1, 1};
+    if (count(all(S), '(') != count(all(S), ')')) {
+        print(mx);
+        print(ans);
+        return 0;
+    }
+
+    rep(i, 0, N) {
+        rep(j, i+1, N) {
+            swap(S[i], S[j]);
+            res = check(S);
+            if (res > mx) {
+                mx = res;
+                ans = {i+1, j+1};
+            }
+            swap(S[i], S[j]);
+        }
+    }
+
+    print(mx);
+    print(ans);
+    return 0;
+}
