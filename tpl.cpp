c503543e2fd052e1dda917ea2555f150f4d707ef
Coki <srcn4frdm.c@gmail.com>
2019-12-03 23:55:05 +0800

    テンプレート更新

--- tpl.cpp
+++ tpl.cpp
@@ -2,34 +2,52 @@
 using namespace std;
 
 typedef long long ll;
-typedef tuple<ll, int, int> tlii;
 typedef pair<ll, ll> pll;
+typedef pair<ll, int> pli;
 typedef pair<int, int> pii;
+typedef pair<pii, ll> ppiil;
+typedef pair<pii, int> ppiii;
 typedef vector<vector<ll>> vvl;
 typedef vector<vector<int>> vvi;
 #define rep(i, a, b) for (ll i=(a); i<(b); i++)
 #define rrep(i, a, b) for (ll i=(a); i>(b); i--)
-#define all(obj) (obj).begin(), (obj).end()
+#define btoe(obj) (obj).begin(), (obj).end()
 #define pb push_back
 #define str to_string
 #define mkp make_pair
-#define mkt make_tuple
-// #define print(out) cout << (out) << endl
 const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
 void chmax(ll &a, ll b) { a = max(a, b); }
 void chmin(ll &a, ll b) { a = min(a, b); }
 
-template<typename Any>
-void print(Any out) {
+template<typename T>
+void print(T out) {
     cout << out << '\n';
 }
 
-void print(vector<ll> A) {
+template<typename T1, typename T2>
+void print(pair<T1, T2> out) {
+    cout << out.first << ' ' << out.second << '\n';
+}
+
+template<typename T>
+void print(vector<T> A) {
     rep(i, 0, A.size()) {
         cout << A[i];
         cout << (i == A.size()-1 ? '\n' : ' ');
     }
+}
+
+template<typename T>
+void print(set<T> S) {
+    vector<T> A(btoe(S));
+    print(A);
+}
+
+ll sum(vector<ll> A) {
+    ll res = 0;
+    for (ll a: A) res += a;
+    return res;
 }
 
 ll max(vector<ll> A) {
@@ -44,6 +62,12 @@ ll min(vector<ll> A) {
     return res;
 }
 
+ll sum(ll A[], int len) {
+    ll res = 0;
+    rep(i, 0, len) res += A[i];
+    return res;
+}
+
 ll max(ll A[], int len) {
     ll res = -INF;
     rep(i, 0, len) chmax(res, A[i]);
@@ -54,6 +78,19 @@ ll min(ll A[], int len) {
     ll res = INF;
     rep(i, 0, len) chmin(res, A[i]);
     return res;
+}
+
+ll toint(string s) {
+    ll res = 0;
+    for (char c : s) {
+        res *= 10;
+        res += (c - '0');
+    }
+    return res;
+}
+
+int toint(char c) {
+    return c - '0';
 }
 
 int main() {
