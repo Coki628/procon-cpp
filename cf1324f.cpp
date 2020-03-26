aadcbbff7c4833f687ee8c100d6c83245a0fc1cf
Coki <srcn4frdm.c@gmail.com>
2020-03-26 22:12:14 +0800

    全方位木DP関連

---
+++ cf1324f.cpp
@@ -0,0 +1,230 @@
+/**
+ * 参考：https://ei1333.hateblo.jp/entry/2018/12/21/004022
+ * 　　　https://codeforces.com/contest/1324/submission/73046463
+ * ・初めての全方位木DP
+ * ・うしさんのライブラリを有難く拝借した。
+ * ・使い方はF1とF2を適切に指定すること。まずはそこから出来るようになっていきたい。
+ * ・F2は新しい辺が入る時に、値がどう変化するか、F1はそれぞれの子ノードをマージ。
+ * ・なんとなくF1が横のマージでF2が縦のマージっていうイメージ。
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ll, int> pli;
+typedef pair<int, int> pii;
+typedef pair<ll, ld> pld;
+typedef pair<pii, int> ppiii;
+typedef pair<pii, ll> ppiil;
+typedef pair<pll, ll> pplll;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define rrep(i, a, b) for (ll i=(a); i>(b); i--)
+#define btoe(obj) (obj).begin(), (obj).end()
+#define pb push_back
+#define str to_string
+#define mkp make_pair
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+template<typename T> inline bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> inline bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T>
+void print(T out) {
+    cout << out << '\n';
+}
+
+void print(ld out) {
+    cout << fixed << setprecision(12) << out << '\n';
+}
+
+template<typename T1, typename T2>
+void print(pair<T1, T2> out) {
+    cout << out.first << ' ' << out.second << '\n';
+}
+
+template<typename T>
+void print(vector<T> A) {
+    rep(i, 0, A.size()) {
+        cout << A[i];
+        cout << (i == A.size()-1 ? '\n' : ' ');
+    }
+}
+
+template<typename T>
+void print(set<T> S) {
+    vector<T> A(btoe(S));
+    print(A);
+}
+
+ll sum(vector<ll> A) {
+    ll res = 0;
+    for (ll a: A) res += a;
+    return res;
+}
+
+ll max(vector<ll> A) {
+    ll res = -INF;
+    for (ll a: A) chmax(res, a);
+    return res;
+}
+
+ll min(vector<ll> A) {
+    ll res = INF;
+    for (ll a: A) chmin(res, a);
+    return res;
+}
+
+ll sum(ll A[], int len) {
+    ll res = 0;
+    rep(i, 0, len) res += A[i];
+    return res;
+}
+
+ll max(ll A[], int len) {
+    ll res = -INF;
+    rep(i, 0, len) chmax(res, A[i]);
+    return res;
+}
+
+ll min(ll A[], int len) {
+    ll res = INF;
+    rep(i, 0, len) chmin(res, A[i]);
+    return res;
+}
+
+ll toint(string s) {
+    ll res = 0;
+    for (char c : s) {
+        res *= 10;
+        res += (c - '0');
+    }
+    return res;
+}
+
+int toint(char c) {
+    return c - '0';
+}
+
+ll pow(ll x, ll n, int mod) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1) {
+            res = (res * x) % mod;
+        }
+        x = (x * x) % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+template<typename sum_t, typename key_t>
+struct ReRooting {
+    struct Edge {
+        int to;
+        key_t data;
+        sum_t dp, ndp;
+    };
+
+    using F1 = function<sum_t(sum_t, sum_t)>;
+    using F2 = function<sum_t(sum_t, key_t)>;
+
+    vector<vector<Edge>> g;
+    vector<sum_t> subdp, dp;
+    const sum_t ident;
+    const F1 f1;
+    const F2 f2;
+
+    ReRooting(int V, const F1 f1, const F2 f2, const sum_t &ident)
+        : g(V), f1(f1), f2(f2), ident(ident), subdp(V, ident), dp(V, ident) {}
+
+    void add_edge(int u, int v, const key_t &d) {
+        g[u].emplace_back((Edge) {v, d, ident, ident});
+        g[v].emplace_back((Edge) {u, d, ident, ident});
+    }
+
+    void add_edge_bi(int u, int v, const key_t &d, const key_t &e) {
+        g[u].emplace_back((Edge) {v, d, ident, ident});
+        g[v].emplace_back((Edge) {u, e, ident, ident});
+    }
+
+    void dfs_sub(int idx, int par) {
+        for(auto &e : g[idx]) {
+        if(e.to == par) continue;
+        dfs_sub(e.to, idx);
+        subdp[idx] = f1(subdp[idx], f2(subdp[e.to], e.data));
+        }
+    }
+
+    void dfs_all(int idx, int par, const sum_t &top) {
+        sum_t buff{ident};
+        for(int i = 0; i < (int) g[idx].size(); i++) {
+        auto &e = g[idx][i];
+        e.ndp = buff;
+        e.dp = f2(par == e.to ? top : subdp[e.to], e.data);
+        buff = f1(buff, e.dp);
+        }
+        dp[idx] = buff;
+        buff = ident;
+        for(int i = (int) g[idx].size() - 1; i >= 0; i--) {
+        auto &e = g[idx][i];
+        if(e.to != par) dfs_all(e.to, idx, f1(e.ndp, buff));
+        e.ndp = f1(e.ndp, buff);
+        buff = f1(buff, e.dp);
+        }
+    }
+
+    vector<sum_t> build() {
+        dfs_sub(0, -1);
+        dfs_all(0, -1, ident);
+        return dp;
+    }
+};
+
+ll N;
+vector<int> A;
+
+ll f1(ll a, ll b) {
+    return a + b;
+}
+
+ll f2(ll a, ll v) {
+    if (A[v]) {
+        return ++a;
+    } else {
+        // 0以下であればここから先を使う必要がないので0にする
+        return max(--a, 0LL);
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cin >> N;
+    A.resize(N);
+    ReRooting<ll, ll> rr(N, f1, f2, 0);
+    rep(i, 0, N) cin >> A[i];
+    ll a, b;
+    rep(i, 0, N-1) {
+        cin >> a >> b;
+        a--; b--;
+        rr.add_edge_bi(a, b, b, a);
+    }
+    vector<ll> ans = rr.build();
+
+    // 最後に自分の頂点の分を足す
+    rep(i, 0, N) {
+        if (A[i]) {
+            ans[i]++;
+        } else {
+            ans[i]--;
+        }
+    }
+    print(ans);
+    return 0;
+}
