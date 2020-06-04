1f1c3f7faf8d7c5c332893c422a50debc0ae1c03
Coki <srcn4frdm.c@gmail.com>
2020-06-04 17:04:33 +0800

    全方位木DP関連

--- edpc_p.cpp
+++ edpc_p.cpp
@@ -1,6 +1,6 @@
 /**
- * ・全方位木DPと木DPの対応
- * ・全方位木DPの理解を深めるため、普通の木DPならどういう動きになるかのDFSを書いておいた。
+ * ・理解を深めるため、全方位木DPと木DPの対応をまた書いておく。
+ * ・前に木DPから全方位起こそうとしてうまくいかなかったやつ、今回はちゃんと書けた。
  */
 
 #include <bits/stdc++.h>
@@ -42,6 +42,7 @@ ll min(vector<ll> A) { ll res = INF; for (ll a: A) chmin(res, a); return res; }
 
 ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
 int toint(char c) { return c - '0'; }
+char tochar(int i) { return '0' + i; }
 
 inline ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
 inline ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
@@ -50,7 +51,9 @@ inline ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a
 inline ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
 
 int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
 
+// 全方位木DP
 template<typename sum_t, typename key_t>
 struct ReRooting {
     struct Edge {
@@ -114,57 +117,28 @@ struct ReRooting {
     }
 };
 
-struct ModTools {
+pll f1(pll a, pll b) {
+    return mkp(a.first * b.first % MOD, a.second * b.second % MOD);
+}
 
-    int MAX;
-    const int MOD;
-    vector<ll> fact, inv;
-
-    ModTools(int MOD) : MOD(MOD) {};
-
-    void build(int mx) {
-        MAX = ++mx;
-        fact.resize(MAX);
-        inv.resize(MAX);
-        fact[0] = fact[1] = 1;
-        rep(i, 2, MAX) {
-            fact[i] = fact[i-1] * i % MOD;
-        }
-        inv[MAX-1] = pow(fact[MAX-1], MOD-2, MOD);
-        rrep(i, MAX-2, -1) {
-            inv[i] = inv[i+1] * (i+1) % MOD;
-        }
-    }
-
-    int nCr(int n, int r) {
-        if (n < r) return 0;
-        r = min(r, n-r);
-        ll num = fact[n];
-        ll den = inv[r] * inv[n-r] % MOD;
-        return num * den % MOD;
-    }
-};
+pll f2(pll a, ll x) {
+    return mkp((a.first + a.second) % MOD, a.first);
+}
 
 ll N;
-ModTools mt(MOD);
-vector<pll> dp;
 vvl nodes;
-
-pll f1(pll a, pll b) {
-    // pair(a内での通り数 * b内での通り数 * a,b全体での並べ方, aの頂点数 + b頂点数)
-    return mkp(a.first * b.first % MOD * mt.nCr(a.second + b.second, a.second) % MOD, a.second + b.second);
-}
-
-pll f2(pll a, ll v) {
-    return mkp(a.first, a.second + 1);
-}
+// dp[i].first  := 頂点iを白にしてその先の全体を塗り分ける通り数
+// dp[i].second := 頂点iを黒にしてその先の全体を塗り分ける通り数
+vector<pll> dp;
 
 void dfs(ll u, ll prev) {
     for (ll v : nodes[u]) {
         if (v == prev) continue;
         dfs(v, u);
-        dp[u].first *= dp[v].first * mt.nCr(dp[u].second + dp[v].second + 1, dp[v].second + 1);
-        dp[u].second += dp[v].second + 1;
+        dp[u].first *= dp[v].first + dp[v].second;
+        dp[u].second *= dp[v].first;
+        dp[u].first %= MOD;
+        dp[u].second %= MOD;
     }
 }
 
@@ -173,24 +147,32 @@ int main() {
     ios::sync_with_stdio(false);
 
     cin >> N;
-    dp.resize(N);
     nodes.resize(N);
-    mt.build(N);
+    dp.resize(N);
+    ReRooting<pll, ll> rr(N, f1, f2, mkp(1, 1));
     ll a, b;
     rep(i, 0, N-1) {
         cin >> a >> b;
         a--; b--;
-        nodes[a].pb(b);
-        nodes[b].pb(a);
+        rr.add_edge(a, b, 1);
+        // nodes[a].pb(b);
+        // nodes[b].pb(a);
     }
-    
-    rep(i, 0, N) {
-        rep(j, 0, N) {
-            dp[j].first = 1; 
-            dp[j].second = 0;
-        }
-        dfs(i, -1);
-        print(dp[i].first);
+    vector<pll> res = rr.build();
+    vector<ll> ans;
+    // rep(i, 0, N) {
+    //     rep(j, 0, N) {
+    //         dp[j].first = dp[j].second = 1;
+    //     }
+    //     dfs(i, -1);
+    //     ans.pb(dp[i].first + dp[i].second);
+    // }
+    rep(i, 0, N) ans.pb((res[i].first + res[i].second) % MOD);
+    sort(ans.begin(), ans.end());
+    auto itr = unique(ans.begin(), ans.end());
+    ans.erase(itr, ans.end());
+    if (ans.size() == 1) {
+         print(ans[0]);
     }
     return 0;
 }
