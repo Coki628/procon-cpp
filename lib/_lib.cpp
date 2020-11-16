9a45040e2dd61bf4a2bc337f64459168acbf66e0
Coki <srcn4frdm.c@gmail.com>
2020-11-16 21:10:59 +0800

    ライブラリ更新

--- lib/_lib.cpp
+++ lib/_lib.cpp
@@ -1,29 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<ll, int> pli;
-typedef pair<int, int> pii;
-typedef pair<ll, ld> pld;
-typedef pair<pii, int> ppiii;
-typedef pair<pii, ll> ppiil;
-typedef pair<pll, ll> pplll;
-typedef pair<pli, int> pplii;
-typedef vector<vector<ll>> vvl;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<pll>> vvpll;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
 #define rep(i, a, b) for (ll i=(a); i<(b); i++)
 #define rrep(i, a, b) for (ll i=(a); i>(b); i--)
 #define pb push_back
 #define tostr to_string
-#define mkp make_pair
 #define list2d(name, N, M, type, init) vector<vector<type>> name(N, vector<type>(M, init))
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
+constexpr ll INF = LONG_LONG_MAX;
+constexpr ll MOD = 1000000007;
 
-void print(ld out) { cout << fixed << setprecision(12) << out << '\n'; }
+void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
+void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
 template<typename T> void print(T out) { cout << out << '\n'; }
 template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.first << ' ' << out.second << '\n'; }
 template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
@@ -37,15 +31,18 @@ ll max(vector<ll> A) { ll res = -INF; for (ll a: A) chmax(res, a); return res; }
 ll min(vector<ll> A) { ll res = INF; for (ll a: A) chmin(res, a); return res; }
 
 ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
-int toint(char c) { return c - '0'; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
 
 inline ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
 inline ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
 
 inline ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
 inline ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+pll divmod(ll a, ll b) { ll d = a / b; ll m = a % b; return {d, m}; }
 
 int popcount(ll S) { return __builtin_popcountll(S); }
+ll gcd(ll a, ll b) { return __gcd(a, b); }
 
 
 // ここから関数
@@ -127,16 +124,16 @@ ll bisearch_max(ll mn, ll mx, function<bool(ll)> func) {
 // 座標圧縮
 typedef unordered_map<ll, int> mli;
 typedef unordered_map<int, ll> mil;
-pair<mli, mil> compress(unordered_set<ll> S) {
+pair<mli, mil> compress(vector<ll> A) {
     mli zipped;
     mil unzipped;
-    vector<ll> A(S.begin(), S.end());
     sort(A.begin(), A.end());
+    A.erase(unique(A.begin(), A.end()), A.end());
     rep(i, 0, A.size()) {
         zipped[A[i]] = i;
         unzipped[i] = A[i];
     }
-    return mkp(zipped, unzipped);
+    return {zipped, unzipped};
 }
 
 // グリッドBFS
@@ -241,25 +238,33 @@ void transpose(vector<vector<T>> &grid) {
 
 struct UnionFind {
 
-    int n;
+    int n, groupcnt;
     vector<int> par, rank, size;
 
     UnionFind(int n) : n(n) {
-        par.resize(n+1);
-        rank.resize(n+1);
-        size.resize(n+1, 1);
-        rep(i, 0, n+1) par[i] = i;
+        par.resize(n);
+        rank.resize(n);
+        size.resize(n, 1);
+        rep(i, 0, n) par[i] = i;
+        groupcnt = n;
     }
 
     UnionFind() {}
 
+    void resize(int _n) {
+        n = _n;
+        par.resize(n);
+        rank.resize(n);
+        size.resize(n, 1);
+        rep(i, 0, n) par[i] = i;
+        groupcnt = n;
+    }
+
     // 根の検索(グループ番号)
     int find(int x) {
-        // 根ならその番号を返す
         if (par[x] == x) {
             return x;
         } else {
-            // 走査していく過程で親を書き換える
             par[x] = find(par[x]);
             return par[x];
         }            
@@ -267,17 +272,16 @@ struct UnionFind {
 
     // 併合
     void merge(int a, int b) {
-        // 根を探す
         int x = find(a);
         int y = find(b);
-        // 木の高さを比較し、低いほうから高いほうに辺を張る
+        if (x == y) return;
+        groupcnt--;
         if (rank[x] < rank[y]) {
             par[x] = y;
             size[y] += size[x];
         } else {
             par[y] = x;
             size[x] += size[y];
-            // 木の高さが同じなら片方を1増やす
             if (rank[x] == rank[y]) {
                 rank[x] += 1;
             }
@@ -296,12 +300,7 @@ struct UnionFind {
 
     // 集合の数
     int get_size() {
-        set<int> res;
-        rep(i, 0, n+1) {
-            res.insert(find(i));
-        }
-        // グループ0の分を引いて返却
-        return res.size() - 1;
+        return groupcnt;
     }
 };
 
@@ -420,7 +419,7 @@ struct SegTree {
     }
 
     // 区間[l,r]で左から最初にxに対して比較の条件を満たすような値が出現する位置
-    ll bisearch_fore(ll l, ll r, ll x, function<bool(ll, ll)> compare) {
+    template<typename G> ll bisearch_fore(ll l, ll r, ll x, const G &compare) {
         ll ok = r + 1;
         ll ng = l - 1;
         while (ng+1 < ok) {
@@ -439,7 +438,7 @@ struct SegTree {
     }
 
     // 区間[l,r]で右から最初にxに対して比較の条件を満たすような値が出現する位置
-    ll bisearch_back(ll l, ll r, ll x, function<bool(ll, ll)> compare) {
+    template<typename G> ll bisearch_back(ll l, ll r, ll x, const G &compare) {
         ll ok = l - 1;
         ll ng = r + 1;
         while (ok+1 < ng) {
