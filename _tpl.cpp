d1f063e7ab8645ed9c71df34404de868249a3299
Coki <srcn4frdm.c@gmail.com>
2020-11-17 17:31:04 +0800

    テンプレート更新

--- _tpl.cpp
+++ _tpl.cpp
@@ -8,27 +8,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<ll, int> pli;
-typedef pair<int, int> pii;
-typedef pair<ll, ld> pld;
-typedef pair<pii, int> ppiii;
-typedef pair<pii, ll> ppiil;
-typedef pair<pll, ll> pplll;
-typedef pair<pli, int> pplii;
-typedef vector<vector<ll>> vvl;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<pll>> vvpll;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vvl = vector<vector<ll>>;
+using vvi = vector<vector<int>>;
+using vvpll = vector<vector<pll>>;
 #define rep(i, a, b) for (ll i=(a); i<(b); i++)
 #define rrep(i, a, b) for (ll i=(a); i>(b); i--)
 #define pb push_back
 #define tostr to_string
-#define mkp make_pair
 #define list2d(name, N, M, type, init) vector<vector<type>> name(N, vector<type>(M, init))
-const ll INF = LONG_LONG_MAX;
-const ll MOD = 1000000007;
+constexpr ll INF = LONG_LONG_MAX;
+constexpr ll MOD = 1000000007;
 
 void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
 void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
@@ -45,9 +38,8 @@ ll max(vector<ll> A) { ll res = -INF; for (ll a: A) chmax(res, a); return res; }
 ll min(vector<ll> A) { ll res = INF; for (ll a: A) chmin(res, a); return res; }
 
 ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
-// 数字なら'0'、アルファベットなら'a'みたいに使い分ける
-// int toint(char c) { return c - '0'; }
-// char tochar(int i) { return '0' + i; }
+int toint(char num) { return num - '0'; }
+char tochar(int num) { return '0' + num; }
 
 inline ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
 inline ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
