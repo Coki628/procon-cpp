440136c81606698b0f4a542c31d0f8517e9bde76
Coki <srcn4frdm.c@gmail.com>
2020-12-14 09:51:33 +0800

    updated templates

--- _tpl-cf.cpp
+++ _tpl-cf.cpp
@@ -19,9 +19,12 @@ using vvpll = vector<vector<pll>>;
 #define rrep(i, a, b) for (ll i=(a); i>(b); i--)
 #define pb push_back
 #define tostr to_string
-#define list2d(name, N, M, type, init) vector<vector<type>> name(N, vector<type>(M, init))
+#define ALL(A) A.begin(), A.end()
 constexpr ll INF = LONG_LONG_MAX;
 constexpr ll MOD = 1000000007;
+
+template<typename T> vector<vector<T>> list2d(int N, int M, T init) { vector<vector<T>> res(N, vector<T>(M, init)); return res; }
+template<typename T> vector<vector<vector<T>>> list3d(int N, int M, int L, T init) { vector<vector<vector<T>>> res(N, vector<vector<T>>(M, vector<T>(L, init))); return res; }
 
 void print(ld out) { cout << fixed << setprecision(15) << out << '\n'; }
 void print(double out) { cout << fixed << setprecision(15) << out << '\n'; }
@@ -30,23 +33,29 @@ template<typename T1, typename T2> void print(pair<T1, T2> out) { cout << out.fi
 template<typename T> void print(vector<T> A) { rep(i, 0, A.size()) { cout << A[i]; cout << (i == A.size()-1 ? '\n' : ' '); } }
 template<typename T> void print(set<T> S) { vector<T> A(S.begin(), S.end()); print(A); }
 
-template<typename T> inline bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
-template<typename T> inline bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+void Yes() { print("Yes"); }
+void No() { print("No"); }
+void YES() { print("YES"); }
+void NO() { print("NO"); }
 
-ll sum(vector<ll> A) { ll res = 0; for (ll a: A) res += a; return res; }
-ll max(vector<ll> A) { ll res = -INF; for (ll a: A) chmax(res, a); return res; }
-ll min(vector<ll> A) { ll res = INF; for (ll a: A) chmin(res, a); return res; }
+ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
+ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
+pll divmod(ll a, ll b) { ll d = a / b; ll m = a % b; return {d, m}; }
+template<typename T> bool chmax(T &x, T y) { return (y > x) ? x = y, true : false; }
+template<typename T> bool chmin(T &x, T y) { return (y < x) ? x = y, true : false; }
+
+template<typename T> T sum(vector<T> A) { T res = 0; for (T a: A) res += a; return res; }
+template<typename T> T max(vector<T> A) { return *max_element(ALL(A)); }
+template<typename T> T min(vector<T> A) { return *min_element(ALL(A)); }
 
 ll toint(string s) { ll res = 0; for (char c : s) { res *= 10; res += (c - '0'); } return res; }
 int toint(char num) { return num - '0'; }
 char tochar(int num) { return '0' + num; }
+int ord(char c) { return (int)c; }
+char chr(int a) { return (char)a; }
 
-inline ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
-inline ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
-
-inline ll floor(ll a, ll b) { if (a < 0) { return (a-b+1) / b; } else { return a / b; } }
-inline ll ceil(ll a, ll b) { if (a >= 0) { return (a+b-1) / b; } else { return a / b; } }
-pll divmod(ll a, ll b) { ll d = a / b; ll m = a % b; return {d, m}; }
+ll pow(int x, ll n) { ll res = 1; rep(_, 0, n) res *= x; return res; }
+ll pow(ll x, ll n, int mod) { ll res = 1; while (n > 0) { if (n & 1) { res = (res * x) % mod; } x = (x * x) % mod; n >>= 1; } return res; }
 
 int popcount(ll S) { return __builtin_popcountll(S); }
 ll gcd(ll a, ll b) { return __gcd(a, b); }
